f7cb5bdd4c2ce2a6dfb8abf7575c16a81f8bda08
fewolee <chulwoo25@naver.com>
2024-11-22 17:54:04 +0900

    [Feat] 프로젝트 세팅

---
+++ src/main/java/com/Dodutch_Server/DodutchServerApplication.java
@@ -0,0 +1,13 @@
+package com.Dodutch_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodutchServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodutchServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Dodutch_Server/DodutchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dodutch_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodutchServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
