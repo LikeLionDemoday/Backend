9accd09e9f7fcbd8f1b1a5b21c894221de61f062
yeobi <kimhyjjang@gmail.com>
2024-12-16 23:58:33 +0900

    [Feat] CI 테스트 환경 H2 데이터베이스 설정 추가

--- src/test/java/com/Dodutch_Server/DodutchServerApplicationTests.java
+++ src/test/java/com/Dodutch_Server/DodutchServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.Dodutch_Server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DodutchServerApplicationTests {
 
 	@Test
