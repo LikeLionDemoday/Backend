2184cf8bca3f1b86c873e617485a8303bedefeed
fewolee <chulwoo25@naver.com>
2024-12-19 07:18:44 +0900

    [Feat] Swagger 설정 구현

---
+++ src/main/java/com/Dodutch_Server/global/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.Dodutch_Server.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("두더치 API")
+                        .description("구현된 API 테스트 해보기")
+                        .version("1.0.0"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")));
+    }
+}
