29f99d3b82ce65f251dc4385920acdbfb24ff002
fewolee <chulwoo25@naver.com>
2024-12-29 20:36:51 +0900

    [Refac] 시큐리티 수정

--- src/main/java/com/Dodutch_Server/global/config/security/CorsConfig.java
+++ src/main/java/com/Dodutch_Server/global/config/security/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig  implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://dodutch.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
